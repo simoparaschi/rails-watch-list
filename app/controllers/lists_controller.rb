19efb7f2ac04f4fb8e83f3f7866d44ad1ce879dc
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-01-16 16:01:53 +0100

    Displaying list with their movies

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,10 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
+
+  def show
+    @id = params[:id]
+    @list = List.find(@id)
+    @movies = @list.movies
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :lists, only: [:index, :show, :new, :create]
   resources :bookmarks
-  resources :lists
 end
