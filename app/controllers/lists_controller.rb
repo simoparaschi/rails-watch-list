42ee828e1bcaad5eae443698c6b69884cfee6fad
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-01-16 16:06:06 +0100

    Can create new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,4 +8,20 @@ class ListsController < ApplicationController
     @list = List.find(@id)
     @movies = @list.movies
   end
+
+  def new
+  @list = List.new
+  end
+
+  def create
+    @list = List.new(form_params)
+    @list.save
+    redirect_to lists_path
+  end
+
+  private
+
+  def form_params
+    params.require(:list).permit(:name)
+  end
 end
