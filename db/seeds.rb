b637e52484af9097e733fc711f3f2582ab11c199
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-01-17 15:05:45 +0100

    Added seeds via API

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,30 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-10.times do
-  rating = rand(0..10)
+# 10.times do
+#   rating = rand(0..10)
+#   movie = Movie.new(
+#     title: Faker::Movie.title,
+#     overview: Faker::Movies::VForVendetta.quote,
+#     poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
+#     rating: rating
+#   )
+#   movie.save!
+# end
+
+require "json"
+require "open-uri"
+url = "https://tmdb.lewagon.com/movie/top_rated"
+user_serialized = URI.parse(url).read
+user = JSON.parse(user_serialized)
+
+
+user["results"].each do |item|
   movie = Movie.new(
-    title: Faker::Movie.title,
-    overview: Faker::Movies::VForVendetta.quote,
-    poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
-    rating: rating
+    title: item["original_title"],
+    overview: item["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{item["poster_path"]}",
+    rating: item["vote_average"]
   )
   movie.save!
 end
